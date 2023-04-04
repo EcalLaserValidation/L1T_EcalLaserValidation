adb57ae6a9e7194bcc44fb974e7ffdfb8088b98d
Zghiche <amina.zghiche@cern.ch>
2023-04-04 16:39:38 +0200

    4000 events/file

--- L1RateValidation_2023.sh
+++ L1RateValidation_2023.sh
@@ -226,7 +226,7 @@ cmsDriver.py l1NtupleRAWEMU_2018 -s RAW2DIGI --era=Run2_2018  \
   --filein=inputFiles \
   --python_filename=l1Ntuple_${GT}.py
 :'
-cmsDriver.py l1Ntuple -s RAW2DIGI --python_filename=l1Ntuple_${GT}.py -n 1000 \
+cmsDriver.py l1Ntuple -s RAW2DIGI --python_filename=l1Ntuple_${GT}.py -n 4000 \
 	     --no_output --no_exec --era=Run3 --data --conditions=$GT \
 	     --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW \
 	     --customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_2022_v0_6 \
