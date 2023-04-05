81d517dc39ed97d3ba2859c64d98a5d6f79cd099
Zghiche <amina.zghiche@cern.ch>
2023-04-05 14:58:15 +0200

    test

--- L1RateValidation_2023.sh
+++ L1RateValidation_2023.sh
@@ -226,7 +226,7 @@ cmsDriver.py l1NtupleRAWEMU_2018 -s RAW2DIGI --era=Run2_2018  \
   --filein=inputFiles \
   --python_filename=l1Ntuple_${GT}.py
 :'
-cmsDriver.py l1Ntuple -s RAW2DIGI --python_filename=l1Ntuple_${GT}.py -n 4000 \
+cmsDriver.py l1Ntuple -s RAW2DIGI --python_filename=l1Ntuple_${GT}.py -n 100 \
 	     --no_output --no_exec --era=Run3 --data --conditions=$GT \
 	     --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW \
 	     --customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_2022_v0_6 \
@@ -243,6 +243,7 @@ Nsq=`echo $sqs | awk -F ' ' '{print NF}'`
 Nfiles=${#filelist[@]}
 NfpJ=`echo "${Nfiles} *${Nsq}/${nproc}" | bc`
 NJ=`echo "${Nfiles}/${NfpJ}" | bc`
+NJ=1
 for sq in $sqs; do
   if [ ! -f EcalTPG_${sq}_moved_to_1.db ]; then
     wget http://cern.ch/ecaltrg/EcalLin/EcalTPG_${sq}_moved_to_1.db
@@ -265,7 +266,8 @@ printf "Execution time to L1Ntuple production: %.6f minutes" $dur
 for sq in $sqs; do
   ls $PWD/L1Ntuple_${GT}_${sq}_*.root > L1Ntuple_${GT}_${sq}.list
   cp $PWD/L1Ntuple_${GT}_${sq}_*.log ${WORKSPACE}/upload/${2}/.
-  #cp $PWD/L1Ntuple_${GT}_${sq}_*.root ${WORKSPACE}/upload/${2}/.
+  #cp $PWD/L1Ntuple_${GT}_${sq}_*.root ${WORKSPACE}/upload/${2}/. 
+  cp $PWD/L1Ntuple_${GT}_${sq}_*.py ${WORKSPACE}/upload/${2}/.
 done
 
 ################################
